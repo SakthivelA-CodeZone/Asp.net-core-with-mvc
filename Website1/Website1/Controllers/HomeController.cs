d3c6689cf80ff23280a85256d64e10e57e7bfb0e
SAKTHIVEL A <sakthiwebnet@gmail.com>
2025-10-19 20:36:16 +0530

    Practice 1(basics of mvc)

---
+++ Website1/Website1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website1/Website1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
